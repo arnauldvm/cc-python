4c6512b30b5e7315aa4306a6c69b140c45b43909
Arnauld Van Muysewinkel <Arnauld.VanMuysewinkel@smals.be>
2023-06-06 14:38:39 +0200

    fix(hooks/pre_gen_project.py): python lint (lines too long)

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -13,8 +13,11 @@ context = json.loads("""
 rules = {
     'project_slug': r'^\S+$',  # avoid spaces
     'author_email': r'^(\S+)@(\S+)\.(\S+)$',
-    'dist_name': r'(?i)^([A-Z0-9]|[A-Z0-9][A-Z0-9._-]*[A-Z0-9])$',  # PEP-423; PEP508 ^(?i)([A-Z0-9]|[A-Z0-9][A-Z0-9._-]*[A-Z0-9])$
-    'pack_name': r'^[a-z][a-z0-9]*$',  # PEP8 (lowercase, no '_') & https://docs.python.org/3/reference/lexical_analysis.html#identifiers
+    'dist_name': r'(?i)^([A-Z0-9]|[A-Z0-9][A-Z0-9._-]*[A-Z0-9])$',
+    #   PEP-423; PEP508 ^(?i)([A-Z0-9]|[A-Z0-9][A-Z0-9._-]*[A-Z0-9])$
+    'pack_name': r'^[a-z][a-z0-9]*$',
+    #   PEP8 (lowercase, no '_')
+    #   & https://docs.python.org/3/reference/lexical_analysis.html#identifiers
     'cli_cmd_name': r'^\S+$',  # avoid spaces
     'with_venv': r'^(yes|no)$',
     'with_git': r'^(yes|no)$',
